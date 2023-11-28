9cd96683a96a14c73faae2e9a4f65e680d830a89
Alesha <121282755+oAleshao@users.noreply.github.com>
2023-11-28 21:06:35 +0300

    All Changes

--- js.js
+++ js.js
@@ -1,22 +1,63 @@
 "use strict"
 
 let startPosition = 95;
+let width = 80;
+
+
 
 start.onclick = () =>{
+
+    let blockZ = document.createElement("div");
+
+    blockZ.style.position = "absolute";
+    blockZ.style.top = "25%";
+    blockZ.style.left =  startPosition + "%";
+
+
+    let number = Math.floor(Math.random() * 7);
     let z = document.createElement("img");
     z.id = "zombi";
-    z.src = "img/3.png";
+    z.src = `img/${number}.png`;
     z.style.height = "130px";
     z.style.width = "80px";
-    z.style.position = "absolute";
-    z.style.top = "25%";
-    z.style.left =  startPosition + "%";
-    fealdGame.append(z);
-    setInterval(() => {
-        z.style.left = --startPosition + "%"
+   
+    let health = document.createElement("div");
+    health.id = "zomblie"
+    health.style.border = "1px solid green";
+    health.style.borderRadius = "10px"
+    health.style.width = "80px";
+    health.style.height = "10px";
+    health.style.background = "lime";
+    blockZ.append(health);
+    blockZ.append(z);
+
+    
+  
+    fealdGame.append(blockZ);
+    let idInterval = setInterval(() => {
+        blockZ.style.left = --startPosition + "%"
+        console.log("WORK");
     }, 800);
+
+    blockZ.onclick = () => {
+       if(width === 0){
+            clearInterval(idInterval);
+            return;
+        }
+        width -= 5;
+        health.style.width = (width) + "px";
+        
+    }
+
+    
 };
 
+document.addEventListener("contextmenu", (event)=>
+{
+    event.preventDefault();
+
+}
+);
 
 
 //fealdGame.
